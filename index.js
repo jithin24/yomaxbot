91cb0ebff8515c58eed1537dc7e474abd001f5ce
Jithin George <583435@TCSGEGDC.com>
2019-01-11 14:49:22 +0530

    Added a log for debugging

--- index.js
+++ index.js
@@ -112,6 +112,7 @@ server.listen(process.env.port || process.env.PORT || 3978, function() {
 // Listen for incoming activities and route them to your bot main dialog.
 server.post('/api/messages', (req, res) => {
     // Route received a request to adapter for processing
+    console.log(`Received a POST Request`);
     adapter.processActivity(req, res, async (turnContext) => {
         // route to bot activity handler.
         await bot.onTurn(turnContext);
